8be7447bcd607c470e8b322759b048123cc79a22
Peter Eax Ahlers <eax.exe@gmail.com>
2012-06-21 08:32:31 +0200

    Testing phase

--- mac.pl
+++ mac.pl
@@ -1,3 +1,4 @@
+use main;
 use Mac::FSEvents;
 use IO::Select;
 # or use Mac::FSEvents qw(:flags);
@@ -5,7 +6,7 @@ use IO::Select;
 my $fs = Mac::FSEvents->new( {
 	path    => '/Users/Eax/Perl/MatteShare/',       # required, the path to watch
 	latency => 2.0,       # optional, time to delay before returning events
-	since   => 451349510, # optional, return events from this eventId
+#	since   => 451349510, # optional, return events from this eventId
 	flags   => NONE,      # optional, set stream creation flags
 } );
 
@@ -16,7 +17,9 @@ my $sel = IO::Select->new($fh);
 while ( $sel->can_read ) {
 	my @events = $fs->read_events;
 	for my $event ( @events ) {
-		printf "Directory %s changed\n", $event->path;
+#		printf "Directory %s changed\n", $event->path;
+		 printf "Event %d received on path %s, Flags: %s\n", $event->id, $event->path, $event->flags;
+		updated();
 	}
 }
 
---
+++ main.pm
@@ -0,0 +1,11 @@
+# Hi and welcome to whateverthisis.
+package main;
+
+# Variables:
+$git_remote_repo = "";
+$local_folder = "/Users/Eax/Perl/MatteShare";
+
+sub updated {
+#	system("cd ".$local_folder." && git add . && git commit -m ");
+	print "Done";
+}
