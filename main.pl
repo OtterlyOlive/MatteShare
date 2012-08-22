95619e9389a5d3ac8d40bbf8a9e8af1806b1d83c
Peter Eax Ahlers <eax.exe@gmail.com>
2012-08-22 09:47:26 +0200

    Removed unneeded file

--- main.pl
+++
@@ -1,7 +0,0 @@
-# Hi and welcome to whateverthisis.
-
-# Variables:
-$git_remote_repo = "";
-$local_folder = "";
-
-system("cd /Users/Eax/perl/MatteShare && git add . && git commit -m 'Testing Perl'");
